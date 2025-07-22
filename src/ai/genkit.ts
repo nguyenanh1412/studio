daba75627a2d4d1fe4dce9a3a78bbc60b4c67196
Anh Nguyễn <nguyen.anh.dev.97@gmail.com>
2025-07-22 09:47:30 +0000

    làm sao để WEB này có thể chạy đc ở localhost hiện tại khi tôi clone sou

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,5 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import {config} from 'dotenv';
+
+config({path: `.env.local`});
 
 export const ai = genkit({
   plugins: [googleAI()],
